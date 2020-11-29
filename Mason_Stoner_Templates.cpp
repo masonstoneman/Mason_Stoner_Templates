35ca11b2bda5197171eb2ec4002aad9a3ea2ddac
masonstoneman <73145558+masonstoneman@users.noreply.github.com>
2020-11-29 17:01:46 -0500

    Initial Commit of source file
    source file is just the framework of the code, with none of the functions done

---
+++ Mason_Stoner_Templates.cpp
@@ -0,0 +1,34 @@
+// Mason Stoner
+//CIS 1202
+// 11/30/2020
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <class T>
+float half(const T)
+{
+
+}
+
+int half(int i)
+{
+
+}
+
+
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
+}
+
+
